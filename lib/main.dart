2a1c72e8193e467a67f62595e66f22b1695c0f5b
noureddine <bounarda1234@gmail.com>
2025-02-27 17:54:49 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 132, 99, 190),
+              borderRadius: BorderRadius.circular(20)
+            ) ,
+            padding: EdgeInsets.all(25),
+            child: Text("Hello World !!"),
+            ),
+        ) ,
+      ),
     );
   }
-
-
-
 }
