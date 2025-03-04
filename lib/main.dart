656705e4e4cf520c261eef5928cdb6ad987e6485
noureddine <bounarda1234@gmail.com>
2025-03-04 13:09:02 +0100

    navigation commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learnapp/pages/first_page.dart';
+import 'package:learnapp/pages/second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,113 +13,130 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text( "My App Bar"),
-        //   backgroundColor: Colors.white,
-        //   leading: Icon(Icons.menu),
-        //   actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-        // ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              // do somthing, user tapped the container.
-              print("user tapped,");
-            } ,
-            child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.tealAccent,
-              child: Center(child: Text("Tap me!")),
-              ),
-          ),
-        ),
+      home: FirstPage(),
 
-        //GridView
-        //Stack
-        // ListView.builder(
-        //   itemCount:10 ,
-        //   itemBuilder: (context, index) => ListTile(
-        //     title: Text(index.toString()),
-        //   ),
-        //   )
-
-
-        // ListView(
-        //   scrollDirection: Axis.vertical,
-        //   children: [
-        //     // 1st box
-        //     Container(
-        //       height: 350,
-        //       color: Colors.deepOrange,
-        //     ),
-
-        //     // 2nd box
-        //     Container(
-        //       height: 350,
-        //       color: Colors.deepOrange[400],
-        //     ),
-
-        //     // 3rd box
-        //     Container(height: 350, 
-        //     color: Colors.deepOrange[200],
-        //     ),
-        //   ],
-        //)
+      routes: {
+        '/secondpage' : (context) => SecondPage()
+      },
 
 
 
-        // Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     // 1st box  
-        //     Expanded(
-        //       child: Container(
-        //         color: Colors.deepPurple,
-        //         ),
-        //     ),
 
-        //       2nd box
-        //       Expanded(
-        //         flex: 2,
-        //         child: Container(
-        //         color: Colors.deepPurple[400],
-        //         ),
-        //       ),
 
-        //       3rd box
-        //       Expanded(
-        //         child: Container(
-        //         color: Colors.deepPurple[200],
-        //         ),
-        //       ),
-        //   ],
-        // )
-        /*Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 132, 99, 190),
-              borderRadius: BorderRadius.circular(20)
-            ) ,
-            padding: EdgeInsets.all(25),
-            // child: Text(
-            //   "Hello World !!",
-            //   style: TextStyle(
-            //     color : Colors.white,
-            //     fontSize: 17,
-            //   ),
-            //   ),
-            child: Icon(
-              Icons.access_alarm_outlined,
-              size: 35,
-              color: Colors.white,
-              ),
-            ),
-        ) ,*/
-      ),
+
+
+
+
+
+
+
+      // home: Scaffold(
+      //   // appBar: AppBar(
+      //   //   title: Text( "My App Bar"),
+      //   //   backgroundColor: Colors.white,
+      //   //   leading: Icon(Icons.menu),
+      //   //   actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+      //   // ),
+      //   body: Center(
+      //     child: GestureDetector(
+      //       onTap: () {
+      //         // do somthing, user tapped the container.
+      //         print("user tapped,");
+      //       } ,
+      //       child: Container(
+      //         height: 200,
+      //         width: 200,
+      //         color: Colors.tealAccent,
+      //         child: Center(child: Text("Tap me!")),
+      //         ),
+      //     ),
+      //   ),
+
+      //   //GridView
+      //   //Stack
+      //   // ListView.builder(
+      //   //   itemCount:10 ,
+      //   //   itemBuilder: (context, index) => ListTile(
+      //   //     title: Text(index.toString()),
+      //   //   ),
+      //   //   )
+
+
+      //   // ListView(
+      //   //   scrollDirection: Axis.vertical,
+      //   //   children: [
+      //   //     // 1st box
+      //   //     Container(
+      //   //       height: 350,
+      //   //       color: Colors.deepOrange,
+      //   //     ),
+
+      //   //     // 2nd box
+      //   //     Container(
+      //   //       height: 350,
+      //   //       color: Colors.deepOrange[400],
+      //   //     ),
+
+      //   //     // 3rd box
+      //   //     Container(height: 350, 
+      //   //     color: Colors.deepOrange[200],
+      //   //     ),
+      //   //   ],
+      //   //)
+
+
+
+      //   // Column(
+      //   //   mainAxisAlignment: MainAxisAlignment.center,
+      //   //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   //   children: [
+      //   //     // 1st box  
+      //   //     Expanded(
+      //   //       child: Container(
+      //   //         color: Colors.deepPurple,
+      //   //         ),
+      //   //     ),
+
+      //   //       2nd box
+      //   //       Expanded(
+      //   //         flex: 2,
+      //   //         child: Container(
+      //   //         color: Colors.deepPurple[400],
+      //   //         ),
+      //   //       ),
+
+      //   //       3rd box
+      //   //       Expanded(
+      //   //         child: Container(
+      //   //         color: Colors.deepPurple[200],
+      //   //         ),
+      //   //       ),
+      //   //   ],
+      //   // )
+      //   /*Center(
+      //     child: Container(
+      //       height: 300,
+      //       width: 300,
+      //       decoration: BoxDecoration(
+      //         color: const Color.fromARGB(255, 132, 99, 190),
+      //         borderRadius: BorderRadius.circular(20)
+      //       ) ,
+      //       padding: EdgeInsets.all(25),
+      //       // child: Text(
+      //       //   "Hello World !!",
+      //       //   style: TextStyle(
+      //       //     color : Colors.white,
+      //       //     fontSize: 17,
+      //       //   ),
+      //       //   ),
+      //       child: Icon(
+      //         Icons.access_alarm_outlined,
+      //         size: 35,
+      //         color: Colors.white,
+      //         ),
+      //       ),
+      //   ) ,*/
+      // ),
     );
   }
 }
---
+++ lib/pages/first_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("1st Page"),),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Go To Second page"),
+          onPressed: () {
+            Navigator.pushNamed(context, '/secondpage');
+          }
+        )
+      ),
+    );
+  }
+}
